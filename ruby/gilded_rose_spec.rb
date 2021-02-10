ad0694e1b6c9ae77a30c5812b1399e5ac856b4f6
Kiki Dawson <kikiadawson@gmail.com>
2021-02-10 12:47:44 +0000

    Add test - Quality doesn't got below zero

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -9,17 +9,21 @@ describe GildedRose do
       expect(items[0].name).to eq "foo"
     end
 
-    it "decreases sell_in by one" do
-      items = [Item.new("foo", 10, 10)]
+    describe 'standard items' do
+      items = [Item.new("foo", 10, 10), Item.new("bar", 10, 0) ]
       GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq 9
-    end
 
-    it "decreases quality by one" do
-      items = [Item.new("foo", 10, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 9
+      it "decreases sell_in by one before sell by" do
+        expect(items[0].sell_in).to eq 9
+      end
+
+      it "decreases quality by one before sell by" do
+        expect(items[0].quality).to eq 9
+      end
+
+      it 'quality doesnt drop below 0' do
+        expect(items[1].quality).to eq 0
+      end
     end
   end
-
 end
