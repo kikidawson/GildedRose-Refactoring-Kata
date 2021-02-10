bd0ee3b801bf8b1f741b11373427665287020077
Kiki Dawson <kikiadawson@gmail.com>
2021-02-10 14:08:34 +0000

    Refactor backstage passes tests

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -55,27 +55,35 @@ describe GildedRose do
     end
 
     describe "Backstage passes" do
-      items = [ Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 10),Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10), Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10), Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10) ]
-      GildedRose.new(items).update_quality()
+      items = [ Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 10) ]
+      guilded_rose = GildedRose.new(items)
 
       it 'increases in quality if 11 or more days to sell' do
-        expect(items[0].sell_in).to eq 14
+        guilded_rose.update_quality()
+
+        expect(items[0].sell_in).to eq 10
         expect(items[0].quality).to eq 11
       end
 
       it 'increases in quality by 2 if 10 or less days to sell' do
-        expect(items[1].sell_in).to eq 9
-        expect(items[1].quality).to eq 12
+        guilded_rose.update_quality()
+
+        expect(items[0].sell_in).to eq 9
+        expect(items[0].quality).to eq 13
       end
 
       it 'increases in quality by 3 if 5 or less days to sell' do
-        expect(items[2].sell_in).to eq 4
-        expect(items[2].quality).to eq 13
+        5.times { guilded_rose.update_quality() }
+
+        expect(items[0].sell_in).to eq 4
+        expect(items[0].quality).to eq 24
       end
 
       it 'quality drops to 0 after event' do
-        expect(items[3].sell_in).to eq -1
-        expect(items[3].quality).to eq 0
+        5.times { guilded_rose.update_quality() }
+
+        expect(items[0].sell_in).to eq -1
+        expect(items[0].quality).to eq 0
       end
     end
   end
