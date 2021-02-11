7a29ba02ce858ad711cd76f639daa0fed74b2d29
Kiki Dawson <kikiadawson@gmail.com>
2021-02-11 13:43:36 +0000

    Refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,63 +6,47 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      find_logic(item)
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        item.sell_in -= 1
+        change_quality_if_between_zero_and_fifty(item)
+      end
     end
   end
 
   private
 
-  def maximum_quality?(item)
-    item.quality == 50
-  end
-
-  def minimum_quality?(item)
-    item.quality == 0
-  end
-
   def aged_brie_logic(item)
-    if !maximum_quality?(item)
-      item.quality += 1
-    end
-    item.sell_in -= 1
+    item.quality += 1
   end
 
   def backstage_passes_logic(item)
-    if !maximum_quality?(item)
-      if item.sell_in < 1
-        item.quality = 0
-      elsif item.sell_in < 6
-        item.quality += 3
-      elsif item.sell_in < 11
-        item.quality += 2
-      else
-        item.quality += 1
-      end
-    end
-    item.sell_in -= 1
+    return item.quality = 0 if past_sell_by(item)
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 10
+    item.quality += 1 if item.sell_in < 5
   end
 
   def standard_item_logic(item)
-    if item.sell_in > 0
-      item.quality -= 1
-    elsif item.sell_in <= 0
-      if !minimum_quality?(item)
-        item.quality -= 2
-      end
-    end
-    item.sell_in -= 1
+    item.quality -= 1
+    item.quality -= 1 if past_sell_by(item)
   end
 
   def find_logic(item)
-    case item.name
-    when "Sulfuras, Hand of Ragnaros"
-    when "Aged Brie"
+    if item.name == "Aged Brie"
       aged_brie_logic(item)
-    when "Backstage passes to a TAFKAL80ETC concert"
+    elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
       backstage_passes_logic(item)
     else
       standard_item_logic(item)
     end
+  end
+
+  def past_sell_by(item)
+    item.sell_in < 0
+  end
+
+  def change_quality_if_between_zero_and_fifty(item)
+    find_logic(item) if item.quality.between?(1, 49)
   end
 end
 
